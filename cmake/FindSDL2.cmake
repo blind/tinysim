e886ee26efdc184338cb8fcfe96523618a794706
Jay Oster <jay@kodewerx.org>
2016-03-25 02:39:55 -0700

    Quiet FindSDL2.cmake

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -65,8 +65,6 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-message("<FindSDL2.cmake>")
-
 SET(SDL2_SEARCH_PATHS
 	~/Library/Frameworks
 	/Library/Frameworks
@@ -161,9 +159,6 @@ IF(SDL2_LIBRARY_TEMP)
 	SET(SDL2_LIBRARY_TEMP "${SDL2_LIBRARY_TEMP}" CACHE INTERNAL "")
 ENDIF(SDL2_LIBRARY_TEMP)
 
-message("</FindSDL2.cmake>")
-
 INCLUDE(FindPackageHandleStandardArgs)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL2 REQUIRED_VARS SDL2_LIBRARY SDL2_INCLUDE_DIR)
-
