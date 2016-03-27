978610ca02856a06b0ad42bb8421e0e3112f6dc2
Jay Oster <jay@kodewerx.org>
2016-03-27 03:11:52 -0700

    Fixes for arduino_to_cxx macro
    - Fix compiler header includes
    - Fix input lists

--- cmake/ArduinoToCXX.cmake
+++ cmake/ArduinoToCXX.cmake
@@ -8,12 +8,21 @@
 #   add_executable(foo ${SRCS})
 
 macro(arduino_to_cxx output_var input)
+    # Add the current list directory for include searching
+    # This is required when a file is moved to the binary directory
+    include_directories(${CMAKE_CURRENT_LIST_DIR})
+
     foreach(f ${input})
         string(REGEX MATCH "\.ino$" matches ${input})
         if(matches)
+            # Get the new filename
             string(REGEX REPLACE "^(.*)\.ino$" "${CMAKE_CURRENT_BINARY_DIR}/\\1.cpp" newname ${f})
-            string(REGEX REPLACE ${f} ${newname} ${output_var} ${${output_var}})
 
+            # Replace the filename in the output list
+            list(REMOVE_ITEM ${output_var} ${f})
+            list(APPEND ${output_var} ${newname})
+
+            # Move the file into the binary directory
             file(COPY ${f} DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
             file(RENAME ${CMAKE_CURRENT_BINARY_DIR}/${f} ${newname})
         endif(matches)
