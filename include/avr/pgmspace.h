0e6268f5f368f6933ee2b24d7867381a11e1d68c
Jay Oster <jay@kodewerx.org>
2016-04-03 02:52:54 -0700

    Add PSTR() macro to avr/pgmspace.h

--- include/avr/pgmspace.h
+++ include/avr/pgmspace.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define PROGMEM
-
+#define PSTR(str) (str)
 
 extern uint8_t pgm_read_byte( const uint8_t* address );
 extern uint16_t pgm_read_word( const uint16_t* address );
